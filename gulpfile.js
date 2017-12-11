0067ee3434c720893ee07903969f695c1d049497
Ganiev Davlet <Ganiev.Davlet@gmail.com>
2017-12-11 12:30:22 +0300

    start project

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,70 @@
+// import { rename } from 'fs';
+
 const gulp = require('gulp');
 const pug = require('gulp-pug');
+
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+
+const del = require('del');
+
+const browserSync = require('browser-sync').create();
 
 const paths = {
     root: './build',
     templates: {
         pages: 'src/templates/pages/*.pug',
         src: 'src/template/**/*.pug'
+    },
+    styles: {
+        src: 'src/styles/**/*.scss',
+        dest: 'build/assets/styles/'
     }
 }
 
-//pug
+// pug
 function templates() {
     return gulp.src(paths.templates.pages)
         .pipe(pug({pretty: true}))
         .pipe(gulp.dest(paths.root));
 }
 
+// scss
+function styles() {
+    return gulp.src('./src/styles/app.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sourcemaps.write())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(paths.styles.dest))
+}
+
+// очистка
+function clean() {
+    return del(paths.root);
+}
+
+// галповский вотчер
+function watch() {
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.templates.src, templates);
+}
+
+// локальный сервер + livereload
+function server() {
+    browserSync.init({
+        server: paths.root
+    });
+    browserSync.watch(paths.root + '/**/*.*', browserSync.reload);
+}
+
 exports.templates = templates;
+exports.styles = styles;
+exports.clean = clean;
+
+gulp.task('default', gulp.series(
+    clean,
+    gulp.parallel(styles, templates),
+    gulp.parallel(watch, server)
+));
